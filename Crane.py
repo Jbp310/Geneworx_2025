bc2f9dbcf3c24d0a103c9360f63bc13a271841cd
Ashton <ashlewthwaite@gmail.com>
2025-11-30 20:44:07 -0500

    Coding a new mission
    I started the coding for the crane mission.

---
+++ Crane.py
@@ -0,0 +1,25 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
+
+left = Motor(Port.A, positive_direction = Direction.COUNTERCLOCKWISE)
+right = Motor(Port.B, positive_direction = Direction.CLOCKWISE)
+move = DriveBase(left, right, 54, 65)
+
+def agl(degres):
+    turn = (degres * 1.234)
+    return int(turn)
+def i2m(inches) :
+    milimeters = (inches * 25.4)
+    return int(milimeters)
+
+move.straight(i2m(3))
+move.turn(agl(-92))
+move.straight(i2m(15))
+move.turn(agl(30))
+move.straight(i2m(5))
+move.turn(agl(-30))
