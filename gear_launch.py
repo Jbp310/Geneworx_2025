def785bb1a2226dfeac0afb129c6385ba176b34e
williamliuzhang-cpu <williamliuzhang@gmail.com>
2025-12-01 18:06:58 -0500

    DID WORK
    Fixed hub menu (everything on imported file will be run first, so you can only define functions in the imported flel). Did more work on missions/

--- gear_launch.py
+++ gear_launch.py
@@ -7,6 +7,12 @@ from pybricks.tools import wait
 # -------------------------------
 # HUB AND MOTORS
 # -------------------------------
+def angle_adjust(angle):
+    if angle > 0:
+        return angle - angle/90 * 15
+    elif angle < 0:
+        return angle+ angle/90 * 15
+    
 hub = PrimeHub()
 attachment_motor = Motor(Port.F, positive_direction=Direction.CLOCKWISE)
 left_motor = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
@@ -137,7 +143,7 @@ def array(option1 ,direction1, distance1,turn_angle1,correction1,speed1):
         x = x + 1
     
 #run function
-def mission_code_right_side():
+def mission_code_right_side_1():
     array(
     option1 =    [1,3,3,3,3,3,3,2,1,3,1],
     direction1 = [0,0,0,0,0,0,0,"left",0,0,0], 
@@ -146,14 +152,22 @@ def mission_code_right_side():
     correction1 =[3,0,0,0,0,0,0,3,3,0,3],
     speed1=      [800,2500,1600,1600,1600,1600,1600,2000,800,1600,-800],
     )
-def mission_code_left_side():
+def mission_code_left_side_1():
     array(
     option1=     [1,3,1,3],
     direction1 = [0,0,0,0],
-    distance1 =  [320,0,500,0],
-    turn_angle1 =[0,-200,0,200],
+    distance1 =  [400,0,500,0],
+    turn_angle1 =[0,-200,0,210],
     correction1= [0.2,0,0.2,0],
     speed1 =     [800,1000,-800,800],
     )
+def mission_code_left_side_2():
+    array(
+    option1=     [1,1],
+    direction1 = [0,0],
+    distance1 =  [1000,1000],
+    turn_angle1 =[0,0],
+    correction1= [3,3],
+    speed1 =     [800,-800],
+    )
 
-mission_code_left_side()
