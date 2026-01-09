b6383d70e6afd3bbedc28793405b42af0b53f32d
Jbp310 <jacksonpeters6789@gmail.com>
2026-01-09 18:04:26 -0500

    Home Transfer Mission
    Added the Home transfer mission fixed some issues with the gyro-straight dependancy

--- Programming/Gyro_Straight.py
+++ Programming/Gyro_Straight.py
@@ -12,6 +12,40 @@ attachment_motor = Motor(Port.F, positive_direction=Direction.CLOCKWISE)
 left_motor = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
 right_motor = Motor(Port.B, positive_direction=Direction.CLOCKWISE)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=110)
+def gyro_turn(direction, turn_angle=90, max_speed=300, correction=4.0):
+    """
+    Turns the robot to a target angle using proportional control.
+    - direction: 'left' or 'right'
+    - turn_angle: degrees to rotate
+    - max_speed: maximum motor speed (deg/sec)
+    - kP: proportional gain
+    """
+    hub.imu.reset_heading(0)
+    wait(100)
+
+    while True:
+        current_angle = hub.imu.heading()
+        error = turn_angle - abs(current_angle)
+
+        # Stop when close enough
+        if error <= 1:
+            break
+
+        # Proportional speed
+        speedy = max(min(error * correction, max_speed), 100)
+    
+        if direction == "left":
+            left_motor.run(-speedy)
+            right_motor.run(speedy)
+        elif direction == "right":
+            left_motor.run(speedy)
+            right_motor.run(-speedy)
+
+        wait(10)
+
+    left_motor.stop()
+    right_motor.stop()
+    wait(200)
 
 def angle_adjust(angle):
     if angle > 0:
@@ -32,7 +66,7 @@ def gyro_straight(distance, base_speed=300, gain=3.0):
     while True:
         avg_angle = (abs(left_motor.angle()) + abs(right_motor.angle())) / 2
         if avg_angle >= target_angle:
-            gyro_reset()
+            gyro_reset_to_straight()
             break
 
 
