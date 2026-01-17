8d66df58cc659f856eb43775a4767a33ff72ee27
williamliuzhang-cpu <williamliuzhang@gmail.com>
2026-01-17 12:46:27 -0500

    More Competition Things

--- Mission_Code.py
+++ Mission_Code.py
@@ -3,8 +3,18 @@ from pybricks.pupdevices import Motor
 from pybricks.parameters import Port, Direction, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
-from gyro_functions import array, gyro_reset_to_straight
-
+from gyro_functions import array, gyro_reset_to_straight, gyro_turn, gyro_straight
+from pybricks.parameters import Axis
+hub = PrimeHub()
+def mission_code_right_side_1():
+      array(
+    option1=     [],
+    direction1 = [],
+    distance1 =  [],
+    turn_angle1 =[],
+    correction1= [],
+    speed1 =     [],
+    )  
 def mission_code_left_side_1():
     array(
     option1=     [1,3,1,3],
@@ -32,3 +42,29 @@ def mission_code_left_side_3():
     correction1= [3,3,3,3,0,3,3,3,3,3,5],
     speed1 =     [800,2000,-800,800,800,-800,2000,800,-800,800,-1000],
     )
+def mission_code_right_side_1():
+    array(
+    option1 =    [1,3,3,3,3,3,3,2,1,3,1],
+    direction1 = [0,0,0,0,0,0,0,"left",0,0,0], 
+    distance1 =  [350,0,0,0,0,0,0,0,190,0,500],
+    turn_angle1 =[0,-690,690,-690,690,-690,900,48,0,-900,0],
+    correction1 =[3,0,0,0,0,0,0,3,3,0,3],
+    speed1=      [800,2500,1600,1600,1600,1600,1600,2000,800,1600,-800],
+    )
+
+
+
+def home_transfer_left():
+    gyro_straight(400, 800)
+    gyro_turn('right',45)
+    gyro_straight(175,800)
+    gyro_turn('left',45)
+    gyro_straight(30000,800)
+    print("facing left")
+def home_transfer_right():
+    gyro_straight(400, 800)
+    gyro_turn('left',45)
+    gyro_straight(175,800)
+    gyro_turn('right',45)
+    gyro_straight(30000,800)
+    print("facing right")
--- Missions+HubMenu.py
+++ Missions+HubMenu.py
@@ -3,17 +3,17 @@ from pybricks.pupdevices import Motor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, hub_menu
-from Mission_Code import mission_code_left_side_1,mission_code_left_side_2, mission_code_left_side_3
+from Mission_Code import mission_code_left_side_1,mission_code_left_side_2, mission_code_left_side_3, home_transfer_left, home_transfer_right,mission_code_right_side_1
 #MAKE SURE IN THE FILE YOU'RE IMPORTING ONLY DEFINES FUNCTIONS, IMPORTING WILL ACtuaLLY RUN THE WHOLE FILE BEFORE RUNNING THE CODE ON THIS FILE
 selected = None
-selected = hub_menu("A", "B", "C", "D", "E")
+selected = hub_menu("A", "B", "C", "D", "E","F","G")
 while selected != None:
     if selected == "A":
         #ashtons code
         break
     elif selected == "B":
         #functions not yet implemented
-        home_transfer()
+        home_transfer_left()
         break
     elif selected == "C":
         mission_code_left_side_1()
@@ -24,4 +24,9 @@ while selected != None:
     elif selected == "E":
         mission_code_left_side_3()
         break
-        
+    elif selected == "F":
+        home_transfer_right()
+        break
+    elif selected == "G": 
+        mission_code_right_side_1()
+        break
